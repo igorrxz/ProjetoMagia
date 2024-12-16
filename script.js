10796cb9dfb93f6399afe6eda70272cbbc4b9be8
Xima <igorrx24072004@gmail.com>
2024-12-16 10:52:48 -0300

    Um último commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Seleciona os elementos
+const navbar = document.querySelector('.navbar');
+const hamburger = document.querySelector('.hamburger');
+
+// Alterna o menu ao clicar no botão hambúrguer
+hamburger.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+document.querySelectorAll('.menu a').forEach(link => {
+  link.addEventListener('click', function (e) {
+      const targetId = this.getAttribute('href'); // Obtém o destino do link
+      if (targetId.startsWith('#')) { // Apenas para âncoras internas
+          e.preventDefault(); // Previne o comportamento padrão
+          const targetElement = document.querySelector(targetId);
+          if (targetElement) {
+              // Faz o scroll suave
+              window.scrollTo({
+                  top: targetElement.offsetTop - 100, // Ajuste o valor de -100 para considerar a altura da navbar
+                  behavior: 'smooth'
+              });
+          }
+      }
+  });
+});
